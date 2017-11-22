1e6a39e3b10458d75ce74c421bb7345758b70f05
Khalil Mohsin <mohsin.khalil8@gmail.com>
2017-11-22 01:58:12 -0500

    Added more JS and JQuery

--- js/GameControl.js
+++ js/GameControl.js
@@ -1,13 +1,10 @@
 /*jshint bitwise: false*/	
 
 
-var obj = {key1: "file:///C|/TemplateWebsite2/Items/SilverBall.JPG", key2: "file:///C|/TemplateWebsite2/Items/Silverplate.JPG",key3: "file:///C|/TemplateWebsite2/Items/shoehorn.JPG",key4: "file:///C|/TemplateWebsite2/Items/paperweight.JPG"};
 
-var r19 = pickrandom(obj);
+function pickrandom(){
+	var obj = {key1: "file:///C|/TemplateWebsite2/Items/SilverBall.JPG", key2: "file:///C|/TemplateWebsite2/Items/Silverplate.JPG",key3: "file:///C|/TemplateWebsite2/Items/shoehorn.JPG",key4: "file:///C|/TemplateWebsite2/Items/paperweight.JPG"};
 
-console.log(r19);
-
-function pickrandom(obj){
     var keys = Object.keys(obj);
     return obj[keys[ keys.length * Math.random() << 0]];
 }
